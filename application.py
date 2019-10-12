7a0600b576b5f68d48ca39d0b372c15ddf889db2
alexinnis <alex.j.innis@gmail.com>
2019-10-12 11:08:10 -0500

    Update application.py

--- application.py
+++ application.py
@@ -19,55 +19,52 @@ application = app.server
 
 app.layout= html.Div(children=[
 
-                html.Div([
-                          
-                    html.H1(children='3D Plotter',style={'textAlign': 'center'}),
-                    
+                html.Div([                          
+                    html.H1(children='3D Plotter',style={'textAlign': 'center'}),                   
                     html.H3(children='3D Plotting Tool for f(x,y)',style={'textAlign': 'center'}),
                     
-                    html.Div([
-                        html.Label('X-Values',style={'display':'table-cell','font-weight':'bold'}),
-                    
+                    html.Div([                       
+                        html.Label('X-Values',style={'display':'table-cell','font-weight':'bold'}),                   
                         dcc.Input(id='xmin',value=0,type='number',placeholder='x minimum',style=dict(display='table-cell')),
-                        html.Div(id="xmin-val"),
-
-                        dcc.Input(id='xmax',value=10,type='number',placeholder="x maximim",style=dict(display='table-cell')),
-                        html.Div(id="xmax-val"),
-
-                        dcc.Input(id='xi',value=1,type='number',placeholder='x increment',style=dict(display='table-cell')),      
                         
+                        html.Div(id="xmin-val"),
+                        dcc.Input(id='xmax',value=10,type='number',placeholder="x maximim",style=dict(display='table-cell')),
+                        
+                        html.Div(id="xmax-val"),
+                        dcc.Input(id='xi',value=1,type='number',placeholder='x increment',style=dict(display='table-cell')),                              
                     ],        
                     style={'display':'inline-block','font-family':'Trebuchet MS, sans-serif'}
                     ),    
+                    
                     html.Div([
                         html.Label('Y-Values',style={'display':'table-cell','font-weight':'bold'}),
-
                         dcc.Input(id='ymin',value=0, type='number',placeholder='y minimum',style=dict(display='table-cell')),
+                        
                         html.Div(id="ymin_val"),
-
                         dcc.Input(id='ymax',value=10,type='number',placeholder='y maximum',style=dict(display='table-cell')),
+                        
                         html.Div(id="ymax_val"),
-
                         dcc.Input(id='yi',value=1,type='number',placeholder='y increment',style=dict(display='table-cell')),        
                     ],        
                     style={'display':'inline-block','padding-left': '20px','font-family':'Trebuchet MS, sans-serif'}
                     ),
+                    
                     html.Div([
                         html.Label('Z-Values for Colorbar',style={'display':'table-cell','font-weight':'bold'}),
-
                         dcc.Input(id='zmin',value=0, type='number',placeholder='z minimum',style=dict(display='table-cell')),
+                        
                         html.Div(id="zmin_val"),
-
                         dcc.Input(id='zmax',value=10,type='number',placeholder='z maximum',style=dict(display='table-cell')),
+                        
                         html.Div(id="zmax_val")       
                     ],        
                     style={'display':'inline-block','padding-left': '20px','font-family':'Trebuchet MS, sans-serif'}
                     ),
+                    
                     html.Div([
                         html.Label('Enter an equation in the form of z = f(x,y)',style={'display':'table-cell','font-weight':'bold'}),
-
                         dcc.Input(id='equation',value='',type='string',placeholder='Example: sin(x**2)*cos(y)',size=50),
-
+                        
                         html.Div(id="equation-val"),
 
                     ],        
@@ -92,8 +89,6 @@ app.layout= html.Div(children=[
                 html.Div(id="graph-val",style={'border':'3px outset','margin-top': '10px','background-color':'#f5f5f5'})    
             ])
 
-
-
 @app.callback(
     Output(component_id='graph-val',component_property='children'),
     [Input(component_id='xmin',component_property='value'),
@@ -109,7 +104,6 @@ app.layout= html.Div(children=[
     ]    
 )
     
-
 def update_data(xminval,xmaxval,xival,yminval,ymaxval,yival,zminval,zmaxval,radio,equation): 
     
     xr = np.arange(xminval,xmaxval,xival) 
